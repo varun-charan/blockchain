ec6e37bd500f24a7d34482cc78c4813a13b26a46
varun-charan <varuncharan1993@gmail.com>
2018-06-02 01:27:36 +0530

    Add files via upload

---
+++ Profile.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [Profile] (  
+	[EMPId] INTEGER  PRIMARY KEY NOT NULL,
+	[EmpName] NVARCHAR(50) NOT NULL, 
+	[Designation] NVARCHAR(50) NOT NULL,   
+	[encash] INTEGER NOT NULL,
+        [reward] INTEGER  NOT NULL
+);
